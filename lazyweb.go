ae6a0d86faf4caca28b28fccccaf3bc3a35e789f
yongbin kim <kybinz@gmail.com>
2016-03-26 23:54:40 +0900

    remove unused localIP()

--- lazyweb.go
+++ lazyweb.go
@@ -5,27 +5,9 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"net"
 	"net/http"
 	"os"
 )
-
-func localIP() string {
-	var iplist []string
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		os.Stderr.WriteString("Error: " + err.Error() + "\n")
-		os.Exit(1)
-	}
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				iplist = append(iplist, ipnet.IP.String())
-			}
-		}
-	}
-	return iplist[0]
-}
 
 type Web struct {
 	Title string
